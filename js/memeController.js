dcdaf5d1191afad339961ba722bb7060dd538a84
Gal Shimer <galshimer2@gmail.com>
2024-06-13 14:14:48 +0300

    render with text

--- js/memeController.js
+++ js/memeController.js
@@ -17,6 +17,17 @@ function renderMeme() {
         gCtx.fillStyle = '#ffffff'
         gCtx.fillRect(0, 0, gCanvas.width, gCanvas.height)
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+
+        var memeText = 'dskjj jsdv skvbkjv'
+        gCtx.fillStyle = '#000000'
+        gCtx.font = '20px Ariel'
+        gCtx.textAlign = 'center'
+        gCtx.fillText (memeText,gCanvas.width/2, 40)
+
+        gCtx.strokeStyle = '#ffffff'
+        gCtx.lineWidth = 2
+        gCtx.strokeText(memeText, gCanvas.width / 2, 40)
+
     }
     img.src = selectedImg.url
 }
